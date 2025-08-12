996dd6a9eb18b3dc4ff60b00386d28e8d21cf7ed
oscarhernandeziv <oscarhernandeziv@gmail.com>
2025-08-12 15:10:03 -0700

    clean up client.tsx

--- src/client.tsx
+++ src/client.tsx
@@ -1,9 +1,5 @@
 import { initClient, initClientNavigation } from "rwsdk/client";
 
-import { suppressHydrationWarning } from "@/lib/utils/hydration";
-
-suppressHydrationWarning();
-
 initClient();
 
 initClientNavigation();
